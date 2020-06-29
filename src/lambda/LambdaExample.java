cdb5e4b60533d67aa2585d9733d44d92cbe93149
sushil.or.k <sushil.or.k@gmail.com>
2020-06-29 13:48:50 +0530

    Initial Commit

---
+++ src/lambda/LambdaExample.java
@@ -0,0 +1,9 @@
+package lambda;
+
+public class LambdaExample {
+    public static void main(String[] args) {
+        // Anonymous Inner classes
+
+        // Lambda Approach
+    }
+}
---
+++ src/list/ListIteration.java
@@ -0,0 +1,16 @@
+package list;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListIteration {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        // Internal Iterator
+
+        // Another form of Internal Iterator
+
+        // Functional Approach using forEach
+
+    }
+}
